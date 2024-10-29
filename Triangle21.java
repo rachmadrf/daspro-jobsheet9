b507b258d50187eded23dca0a581967eefff6318
rachmadrf <rachmad17845@gmail.com>
2024-10-29 21:24:56 +0700

    update

--- Triangle21.java
+++ Triangle21.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class Triangle {
+public class Triangle21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         System.out.print("Masukkan nilai N = ");
